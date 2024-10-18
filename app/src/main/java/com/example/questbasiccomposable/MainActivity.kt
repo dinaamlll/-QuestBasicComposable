4ace4377be82e6bfa024a345d21463ad2b40a33b
dinaamlll <dinamaliaa17@gmail.com>
2024-10-18 16:53:33 +0700

    Menambahkan text "Login" dengan mengatur ukuran text nya menjadi 32.sp dan di pertebal

--- app/src/main/java/com/example/questbasiccomposable/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,8 +13,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.questbasiccomposable.ui.theme.QuestBasicComposableTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,27 +35,35 @@ class MainActivity : ComponentActivity() {
 }
 @Composable
 fun BasicColumn(modifier: Modifier = Modifier) {
-    Column (
+    Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top,
         modifier = Modifier
             .fillMaxSize()
-            .padding(top = 50.dp)
+            .padding(16.dp)
+    ) {
+        // Login Text
+        Text(
+            text = "Login",
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Bold
 
-    )
-}
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+        )
+        @Composable
+        fun Greeting(name: String, modifier: Modifier = Modifier) {
+            Text(
+                text = "Hello $name!",
+                modifier = modifier
+            )
 
-@Preview(showBackground = true)
-@Composable
-fun BasicColumnPreview() {
-    QuestBasicComposableTheme {
-        BasicColumn()
+        }
     }
 }
+        @Preview(showBackground = true)
+        @Composable
+        fun BasicColumnPreview() {
+            QuestBasicComposableTheme {
+                BasicColumn()
+            }
+        }
+
