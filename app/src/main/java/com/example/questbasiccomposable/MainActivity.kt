478ebf72a14e5df07ce89d0484eac630abecc3e3
dinaamlll <dinamaliaa17@gmail.com>
2024-10-18 17:01:05 +0700

    menampilkan gambar logo UMY dengan ukuran 180.dp

--- app/src/main/java/com/example/questbasiccomposable/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable/MainActivity.kt
@@ -4,17 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -58,6 +61,15 @@ fun BasicColumn(modifier: Modifier = Modifier) {
             fontSize = 18.sp
         )
         Spacer(modifier = Modifier.height(40.dp))
+
+        // UMY Logo
+        Image(
+            painter = painterResource(id = R.drawable.logo), // Make sure this image exists
+            contentDescription = "Logo UMY",
+            modifier = Modifier.size(180.dp)
+        )
+        Spacer(modifier = Modifier.height(5.dp))
+
 
         @Composable
         fun Greeting(name: String, modifier: Modifier = Modifier) {
