11199779760459543b9870c0c4cc535d8708e6d3
dinaamlll <dinamaliaa17@gmail.com>
2024-10-18 16:10:19 +0700

    mengatur agar elemen di dalam kolom berada secara horizontal ke tengah dan daiatur secara vertikal

--- app/src/main/java/com/example/questbasiccomposable/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.questbasiccomposable.ui.theme.QuestBasicComposableTheme
@@ -29,7 +31,10 @@ class MainActivity : ComponentActivity() {
 }
 @Composable
 fun BasicColumn(modifier: Modifier = Modifier) {
-
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
+    )
 }
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
